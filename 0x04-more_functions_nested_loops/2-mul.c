7b8fc7ad5c9d97887e46a137cb05cf6d9668ce63
Anduti724 <anndigech@gmail.com>
2021-09-02 14:21:07 -0700

    first commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 /**
- *mul - multiply 2 numbers
- *@a: int
- *@b: int
- *Return: product
- */
-
+*mul - multiply 2 numbers
+*@a: int
+*@b: int
+*Return: product
+*/
 int mul(int a, int b)
 {
-  int product;
-  product = a * b;
-  return (product);
+int product;
+product = a * b;
+return (product);
 }
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,8 +7,9 @@
 
 void print_numbers(void)
 {
-  int num;
-  for (num = 0; num <= 9; num++)
-    {
-      _putchar(num + '0');
-    }
+int num;
+for (num = 0; num <= 9; num++)
+{
+_putchar(num + '0');
+}
+}
