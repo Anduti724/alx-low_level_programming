893487a4ccbf69ee3c19026ad86981c7c0c080f2
Anduti724 <anndigech@gmail.com>
2021-09-02 13:54:32 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *print_numbers - print numbers 0-9
+ *
+ *Return:always 0
+ */
+
+void print_numbers(void)
+{
+  int num;
+  for (num = 0; num <= 9; num++)
+    {
+      _putchar(num + '0');
+    }
